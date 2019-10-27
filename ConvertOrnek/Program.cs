f388035873aef623ff5e6b587abaf5113e388f3e
muratvuranok <muratvuranok@hotmail.com>
2019-10-27 10:56:25 +0300

    Add project files.

---
+++ ConvertOrnek/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertOrnek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Dışarıdan girilen 2 sayının (toplam,fark,çarpım,bölüm ve mod) işlemlerini yapıp kullanıcıya gösteriniz.
+        }
+    }
+}
